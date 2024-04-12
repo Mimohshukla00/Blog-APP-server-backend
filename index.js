652b5f4a8423a992ba49212eb8ef8685fafa7903
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-12 14:05:27 +0530

    server

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ const blog = require("./routes/blog");
 
 // mount
 app.use("/api/v1/blog", blog);
+
+const connectDB = require("./config/database");
+connectDB();
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>who are you bitch</h1>");
+});
