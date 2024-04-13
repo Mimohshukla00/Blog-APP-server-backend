3dc2b39de169cfbfa4e5732e48b5f65568b05ba2
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-04-13 22:06:49 +0530

    commit

--- routes/blog.js
+++ routes/blog.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 // import controller
 const { dummyLink } = require("../controllers/likeController");
+const { createComment } = require("../controllers/commentController");
 
 // routes // Mapping creation
 
 router.get("/dummyroute", dummyLink);
+router.post("/comments/create",createComment);
 
 // export
 
